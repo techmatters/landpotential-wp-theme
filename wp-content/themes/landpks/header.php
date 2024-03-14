c1249a6e421cbc004582da520317169022d94ede
Paul Schreiber <paul@techmatters.org>
2024-03-14 17:35:04 -0400

    fix: fix escaping for page container style

--- wp-content/themes/landpks/header.php
+++ wp-content/themes/landpks/header.php
@@ -38,9 +38,12 @@
 <body <?php body_class(); ?>>
 <?php
 	$product_tour_enabled = et_builder_is_product_tour_enabled();
-	$page_container_style = $product_tour_enabled ? ' style="padding-top: 0px;"' : '';
 ?>
-	<div id="page-container"<?php echo et_core_intentionally_unescaped( $page_container_style, 'fixed_string' ); ?>>
+	<div id="page-container"
+	<?php
+	if ( $product_tour_enabled ) :
+		?>
+style="padding-top: 0px;"<?php endif; ?>>
 <?php
 if ( $product_tour_enabled || is_page_template( 'page-template-blank.php' ) ) {
 	return;
