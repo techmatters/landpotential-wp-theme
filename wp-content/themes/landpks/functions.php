ab888702ea0c6533ac96ca172ec152bfc760edcc
Paul Schreiber <paul@techmatters.org>
2024-03-15 18:49:53 -0400

    fix: require additional classes

--- wp-content/themes/landpks/functions.php
+++ wp-content/themes/landpks/functions.php
@@ -10,3 +10,8 @@ define( 'THEME_VERSION', '1.0.0' );
 
 require_once __DIR__ . '/class-landpks.php';
 require_once __DIR__ . '/includes/class-newsletter.php';
+require_once __DIR__ . '/includes/settings/class-settings.php';
+require_once __DIR__ . '/includes/settings/class-google-settings.php';
+
+// Google integration.
+require_once __DIR__ . '/includes/class-google-recaptcha.php'; // Must be after settings page.
