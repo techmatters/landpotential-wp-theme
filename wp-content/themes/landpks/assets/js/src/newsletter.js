a60b67e509f346d6c5c6154068e73019b8c788bf
Paul Schreiber <paul@techmatters.org>
2024-03-18 12:53:01 -0400

    feat: add AJAX form submission

--- wp-content/themes/landpks/assets/js/src/newsletter.js
+++ wp-content/themes/landpks/assets/js/src/newsletter.js
@@ -13,6 +13,33 @@ window.lpks = {
 		}
 	},
 
+	handleSubmitEvent: function( event ) {
+		event.preventDefault();
+		jQuery.ajax( {
+			type: 'POST',
+			url: localize._ajax_url,
+			data: {
+				_ajax_nonce: localize._ajax_nonce,
+				action: 'newsletter_subscribe',
+				email: event.target.querySelector( 'input[name="email"]' ).value,
+				first_name: event.target.querySelector( 'input[name="first_name"]' ).value,
+				last_name: event.target.querySelector( 'input[name="last_name"]' ).value
+			},
+			success: ( res ) => {
+				if ( true === res.success ) {
+					jQuery( event.target.querySelector( '.form-fields' ) ).slideUp();
+					event.target.querySelector( '.message' ).textContent = localize.success;
+				} else {
+					let errorMessage = localize.error_codes[res.data.error_code];
+					if ( 4 === res.data.error_code ) {
+						errorMessage += ` (${res.data.error_message})`;
+					}
+					event.target.querySelector( '.message' ).textContent = errorMessage;
+				}
+			}
+		} );
+	},
+
 	setup: function() {
 		console.log( 'setting up...' );
 		const subscribeForm = document.getElementById( 'inline-subscribe' );
@@ -20,6 +47,7 @@ window.lpks = {
 		if ( subscribeForm ) {
 			console.log( 'hooked' );
 			window.lpksSubmitSubscribeForm = window.lpks.submitSubscribeForm; // ReCAPTCHA callback needs to be in the global namespace
+			subscribeForm.addEventListener( 'submit', window.lpks.handleSubmitEvent );
 		}
 	}
 };
