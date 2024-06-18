56874e9e3dc41cdca073e2102fc9f435ec554e65
Paul Schreiber <paul@techmatters.org>
2024-06-18 14:22:10 -0400

    fix: on email-only form: add class, fix indesnts, associate label with input

--- wp-content/plugins/lpks-newsletter/template-parts/form-email.php
+++ wp-content/plugins/lpks-newsletter/template-parts/form-email.php
@@ -7,15 +7,14 @@
  */
 
 ?>
-<form id="inline-subscribe" class="newsletter-subscribe" method="post">
+<form id="inline-subscribe" class="newsletter-subscribe email-only" method="post">
 	<div class="form-fields">
-			<label class="screen-reader">Email address</label>
-			<input type="email" name="email" placeholder="Email address" required="required">
-		</div>
-		<div class="signup-input">
-			<input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
-			<input type="submit" class="signup g-recaptcha" value="Subscribe" />
-		</div>
+		<label for="inline-subscribe-email">Email address:</label>
+		<input id="inline-subscribe-email" type="email" name="email" placeholder="Email address" required="required">
+	</div>
+	<div class="signup-input">
+		<input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
+		<input type="submit" class="signup g-recaptcha" value="Subscribe" />
 	</div>
 	<div class="message"></div>
 </form>
