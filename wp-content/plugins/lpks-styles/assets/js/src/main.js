3cf38fa7c51e2d0879617a73629228103c02ff6b
Paul Schreiber <paul@techmatters.org>
2024-07-09 15:28:31 -0400

    feat: add script

---
+++ wp-content/plugins/lpks-styles/assets/js/src/main.js
@@ -0,0 +1,9 @@
+window.lpks = window.lpks || {};
+
+window.lpks.styleMenus = function() {
+	document
+		.querySelector( '.current-menu-item' )
+		?.parentNode?.classList?.add( 'toggled-on' );
+};
+
+document.addEventListener( 'DOMContentLoaded', window.lpks.styleMenus );
