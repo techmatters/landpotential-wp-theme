e01723f8af10a30f771d09a4a4cbe3afa67b690c
Paul Schreiber <paul@techmatters.org>
2024-09-17 13:44:21 -0400

    feat: add help CPT

---
+++ wp-content/plugins/lpks-help/lpks-styles.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * LandPKS Help WordPress plugin.
+ *
+ * @package LPKS
+ */
+
+/**
+ * Plugin Name:       LandPKS Help
+ * Plugin URI:        https://landpotential.org/
+ * Description:       LandPKS Help CPT
+ * Author:            Tech Matters, paulschreiber
+ * Text Domain:       lpks
+ * Domain Path:       /languages
+ * Version:           1.0.0
+ * Requires at least: 6.4
+ *
+ * @package         LPKS
+ */
+
+defined( 'ABSPATH' ) || exit;
+define( 'LPKS_HELP_PLUGIN_VERSION', '1.0.0' );
+
+require_once __DIR__ . '/includes/class-terraso-help-cpt.php';
