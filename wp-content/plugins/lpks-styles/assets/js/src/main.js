614caf87587d497b59cc28d1e2644c762b238737
Paul Schreiber <paul@techmatters.org>
2024-10-16 11:02:10 -0400

    fix: auto-expand grandparent menu

--- wp-content/plugins/lpks-styles/assets/js/src/main.js
+++ wp-content/plugins/lpks-styles/assets/js/src/main.js
@@ -1,9 +1,16 @@
 window.lpks = window.lpks || {};
 
 window.lpks.styleMenus = function() {
-	document
-		.querySelector( '.current-menu-item' )
-		?.parentNode?.classList?.add( 'toggled-on' );
+	const currentItem = document.querySelector( '.current-menu-item' );
+
+	// parent
+	currentItem?.parentNode?.classList?.add( 'toggled-on' );
+
+	// grandparent UL (li > parent ul > li > grandarent ul)
+	const grandParent = currentItem.parentNode.parentNode.parentNode;
+	if ( 'UL' === grandParent.tagName ) {
+		grandParent.classList?.add( 'toggled-on' );
+	}
 };
 
 document.addEventListener( 'DOMContentLoaded', window.lpks.styleMenus );
